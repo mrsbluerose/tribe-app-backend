2bea7921823fbe52a746cec69bc7c91d90ad3fe0
Audra W <37217144+mrsbluerose@users.noreply.github.com>
2024-06-27 13:42:12 -0600

    TRIB-12: update all loggers to use Lombok @Slf4j (#228)
    * TRIB-12: updates ValidationUtil logger to @Slf4j
    
    * TRIB-12: updates StorageService logger to @Slf4j
    
    * TRIB-12: removed logger import in StorageSvcTest
    
    * TRIB-12: removes logger import PictureServiceImpl
    
    * TRIB-12: adds PictureServiceImplTest class
    
    * TRIB-12: updates PictureServiceImpl file separator
    - updates "/" to File.separator for cross platform compatibility
    
    * TRIB-12: adds testPictureConstants for PicSvc test
    
    * TRIB-12: deletes image testing files
    - reasoning: found better testing method
    
    * TRIB-12: refactors PictureServiceImpl with helpers
    - adds helper methods for processing files (getFileSystemResource, readImage, createfile)
    - reasoning: helps make it possible to write tests by moving java file manipulation methods to helper methods.
    
    * TRIB-12: refactors PictureServiceImplTest
    - adds override mocks for PictureServiceImpl java file manipulation helper methods
    - refactors tests
    
    * TRIB-12: refactors PictureServiceImplTests
    - refactors PictureServiceImpl helper methods to be package private, allowing testing access
    - added tests for helper methods
    
    * TRIB-12: moves PictureServiceImpl helper tests
    - creates new test class for PictureServiceHelper methods to avoid testing the overriden methods in PictureServiceImplTest
    
    * TRIB-12: updated PictureSvcImplHelperMethodsTest
    
    * TRIB-12: updates PictureService and tests
    - updates PictureService and PictureServiceImpl to override helper methods
    - refactors PictureServiceImplTests to properly mock all method calls
    
    * small change trigger github workflow
    
    * Update README.md
    
    cleared errant space after 'info@savvato.com'
    
    ---------
    
    Co-authored-by: Johnathan James <haxwell@gmail.com>

--- src/main/java/com/savvato/tribeapp/services/PictureService.java
+++ src/main/java/com/savvato/tribeapp/services/PictureService.java
@@ -1,9 +1,24 @@
 package com.savvato.tribeapp.services;
 
+import org.springframework.core.io.FileSystemResource;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 
 public interface PictureService {
 
+    InputStream getInputStreamFromFileSystemResource(FileSystemResource fileSystemResource) throws IOException;
+
+    FileSystemResource getFileSystemResource(String path);
+
+    BufferedImage readImage(InputStream inputStream) throws IOException;
+
+    File createFile(String path);
+
     String transformFilenameUsingSizeInfo(String photoSize, String filename);
     void writeThumbnailFromOriginal(String resourceType, String filename) throws IOException;
+
+    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException;
 }
--- src/main/java/com/savvato/tribeapp/utils/ValidationUtil.java
+++ src/main/java/com/savvato/tribeapp/utils/ValidationUtil.java
@@ -1,23 +1,21 @@
 package com.savvato.tribeapp.utils;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public final class ValidationUtil {
   private ValidationUtil() {}
 
-  private static final Log logger = LogFactory.getLog(ValidationUtil.class);
-
   public static boolean isPhoneValid(String str) {
 
-    logger.warn("ValidationUtil::isPhoneValid()    NEEDS    TO    BE     IMPLEMENTED    !!!");
+    log.warn("ValidationUtil::isPhoneValid()    NEEDS    TO    BE     IMPLEMENTED    !!!");
 
     return str != null;
   }
 
   public static boolean isEmailValid(String str) {
 
-    logger.warn("ValidationUtil::isEmailValid()    NEEDS    TO    BE     IMPLEMENTED    !!!");
+    log.warn("ValidationUtil::isEmailValid()    NEEDS    TO    BE     IMPLEMENTED    !!!");
 
     return str != null;
   }
