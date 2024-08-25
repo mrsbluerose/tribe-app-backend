b50ce8d39aab0f5223f80f0fe30451eaf07f382b
Audra W <37217144+mrsbluerose@users.noreply.github.com>
2024-08-25 09:56:52 -0600

    TRIB-272: updates testing by separating unit tests and integration tests (#273)
    * TRIB-272: moves all service tests to unit directry
    - refactors package names and imports
    
    * TRIB-272: adds plugin for MVN fail safe
    - allows for integration tests
    
    * TRIB-272: updates pom, moves controller directory
    - updates pom mvn surefire plugin to look for unit directory
    - moves controller tests to the new integration directory
    
    * TRIB-272: renames integration tests
    
    * TRIB-272: updates pom with test reports
    - adds reports
    
    * TRIB-272: updates pom comment
    
    * TRIB-272: refactors UserAPITest
    
    * TRIB-272: updates integration test names
    
    * TRIB-272: refactors SchemaUtilsTest location
    
    * TRIB-272: updates pom jacoco report to run on verify
    
    * TRIB-272: updates codecov yml to run tests on verify

---
+++ src/test/java/com/savvato/tribeapp/unit/services/ConnectServiceImplTest.java
@@ -0,0 +1,426 @@
+package com.savvato.tribeapp.unit.services;
+
+import com.savvato.tribeapp.constants.Constants;
+import com.savvato.tribeapp.constants.UserTestConstants;
+import com.savvato.tribeapp.dto.ConnectOutgoingMessageDTO;
+import com.savvato.tribeapp.dto.GenericResponseDTO;
+import com.savvato.tribeapp.dto.UsernameConnectionStatusDTO;
+import com.savvato.tribeapp.entities.Connection;
+import com.savvato.tribeapp.repositories.ConnectionsRepository;
+import com.savvato.tribeapp.repositories.UserRepository;
+import com.savvato.tribeapp.services.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith({SpringExtension.class})
+public class ConnectServiceImplTest implements UserTestConstants {
+    @TestConfiguration
+    static class ConnectServiceImplTestContextConfiguration {
+        @Bean
+        public ConnectService connectService() {
+            return new ConnectServiceImpl();
+        }
+
+        @Bean
+        public CacheService cacheService() {
+            return new CacheServiceImpl();
+        }
+    }
+
+    @Autowired
+    ConnectService connectService;
+
+    @MockBean
+    CacheService cacheService;
+
+    @MockBean
+    ConnectionsRepository connectionsRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    GenericResponseService genericResponseService;
+
+    @Test
+    public void getQRCodeString() {
+        Long userId = USER1_ID;
+        Optional<String> qrCodeString = Optional.of("QR code");
+        Mockito.when(cacheService.get(Mockito.any(), Mockito.any())).thenReturn(qrCodeString.get());
+        Optional<String> rtn = connectService.getQRCodeString(userId);
+        assertEquals(qrCodeString, rtn);
+    }
+
+    @Test
+    public void storeQRCodeString() {
+        Long userId = USER1_ID;
+        Optional<String> generatedQRCodeString = Optional.of("QR code");
+
+        Optional<String> rtn = connectService.storeQRCodeString(userId);
+
+        ArgumentCaptor<String> arg1 = ArgumentCaptor.forClass(String.class);
+        ArgumentCaptor<String> arg2 = ArgumentCaptor.forClass(String.class);
+        verify(cacheService, times(1)).put(arg1.capture(), arg2.capture(), Mockito.any());
+
+        assertEquals(arg1.getValue(), "ConnectQRCodeString");
+        assertEquals(arg2.getValue(), String.valueOf(userId));
+    }
+
+    @Test
+    public void saveConnectionDetailsHappyPath() {
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+
+        Boolean connectionStatus = connectService.saveConnectionDetails(requestingUserId, toBeConnectedWithUserId);
+        assertEquals(connectionStatus, true);
+    }
+
+    @Test
+    public void saveConnectionDetailsUnhappyPath() {
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        doThrow(new IndexOutOfBoundsException()).when(connectionsRepository).save(Mockito.any());
+        Boolean connectionStatus = connectService.saveConnectionDetails(requestingUserId, toBeConnectedWithUserId);
+        assertEquals(connectionStatus, false);
+    }
+
+    @Test
+    public void testValidateConnectionHappyPath() {
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+
+        when(userService.getLoggedInUserId()).thenReturn(USER1_ID);
+
+        Optional<GenericResponseDTO> validateConnection = connectService.validateConnection(requestingUserId, toBeConnectedWithUserId);
+
+        assertThat(validateConnection.isEmpty());
+        verify(userService, times(1)).getLoggedInUserId();
+
+    }
+
+    @Test
+    public void testValidateConnectionWhenIdsAreTheSame() {
+        Long requestingUserId = USER2_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        GenericResponseDTO expectedGenericResponseDTO = GenericResponseDTO.builder()
+                .booleanMessage(false)
+                .responseMessage("User " + requestingUserId + " may not have a connection to themselves")
+                .build();
+
+        when(userService.getLoggedInUserId()).thenReturn(USER2_ID);
+
+        Optional<GenericResponseDTO> validateConnection = connectService.validateConnection(requestingUserId, toBeConnectedWithUserId);
+
+        assertThat(expectedGenericResponseDTO).usingRecursiveComparison().isEqualTo(validateConnection.get());
+        verify(userService, times(1)).getLoggedInUserId();
+
+    }
+
+    @Test
+    public void testValidateConnectionWhenRequestingUserNotLoggedIn() {
+        Long loggedInUser = USER3_ID;
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        GenericResponseDTO expectedGenericResponseDTO = GenericResponseDTO.builder()
+                .booleanMessage(false)
+                .responseMessage("The logged in user (" + loggedInUser + ") does not match requesting user (" + requestingUserId + ")")
+                .build();
+
+        when(userService.getLoggedInUserId()).thenReturn(loggedInUser);
+
+        Optional<GenericResponseDTO> validateConnection = connectService.validateConnection(requestingUserId,toBeConnectedWithUserId);
+
+        assertThat(expectedGenericResponseDTO).usingRecursiveComparison().isEqualTo(validateConnection.get());
+        verify(userService, times(1)).getLoggedInUserId();
+
+    }
+
+    @Test
+    public void validateQRCode() {
+        ConnectService connectServiceSpy = spy(connectService);
+        Optional<String> qrCodeOpt = Optional.of("ABCDEFGHIJKL");
+        String providedQRCode = "ABCDEFGHIJKL";
+        Long toBeConnectedWithUserId = USER1_ID;
+        doReturn(qrCodeOpt).when(connectServiceSpy).getQRCodeString(anyLong());
+        Boolean isValid = connectServiceSpy.validateQRCode(providedQRCode, toBeConnectedWithUserId);
+        assertTrue(isValid);
+    }
+
+    @Test
+    public void removeConnectionHappyPath() {
+
+        Long requestingUserId = USER1_ID;
+        Long connectedWithUserId = USER2_ID;
+
+        GenericResponseDTO expectedDTO = GenericResponseDTO.builder()
+                .booleanMessage(true)
+                .build();
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(Optional.empty()).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+        doNothing().when(connectionsRepository).delete(Mockito.any());
+
+        ArgumentCaptor<Connection> connectionCaptor = ArgumentCaptor.forClass(Connection.class);
+
+        GenericResponseDTO actualDTO = connectServiceSpy.removeConnection(requestingUserId, connectedWithUserId);
+
+        assertThat(expectedDTO).usingRecursiveComparison().isEqualTo(actualDTO);
+        verify(connectionsRepository, times(1)).delete(connectionCaptor.capture());
+        assertEquals(connectionCaptor.getValue().getRequestingUserId(), requestingUserId);
+        assertEquals(connectionCaptor.getValue().getToBeConnectedWithUserId(), connectedWithUserId);
+    }
+
+    @Test
+    public void removeConnectionWhenDatabaseDeleteFails() {
+
+        Long requestingUserId = USER1_ID;
+        Long connectedWithUserId = USER2_ID;
+
+        GenericResponseDTO expectedDTO = GenericResponseDTO.builder()
+                .responseMessage("Database delete failed.")
+                .booleanMessage(false)
+                .build();
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(Optional.empty()).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+
+        doThrow(new IllegalArgumentException("Database delete failed.")).when(connectionsRepository).delete(Mockito.any());
+
+        ArgumentCaptor<Connection> connectionArgumentCaptor = ArgumentCaptor.forClass(Connection.class);
+
+        GenericResponseDTO actualDTO = connectServiceSpy.removeConnection(requestingUserId, connectedWithUserId);
+
+        assertThat(expectedDTO).usingRecursiveComparison().isEqualTo(actualDTO);
+        verify(connectionsRepository, times(1)).delete(connectionArgumentCaptor.capture());
+        assertEquals(connectionArgumentCaptor.getValue().getRequestingUserId(), requestingUserId);
+        assertEquals(connectionArgumentCaptor.getValue().getToBeConnectedWithUserId(), connectedWithUserId);
+    }
+
+    @Test
+    public void removeConnectionWhenConnectionInvalid() {
+
+        Long requestingUserId = USER1_ID;
+        Long connectedWithUserId = USER1_ID;
+
+        GenericResponseDTO expectedDTO = GenericResponseDTO.builder()
+                .responseMessage("message")
+                .booleanMessage(false)
+                .build();
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(Optional.of(expectedDTO)).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+
+        GenericResponseDTO actualDTO = connectServiceSpy.removeConnection(requestingUserId, connectedWithUserId);
+
+        assertThat(expectedDTO).usingRecursiveComparison().isEqualTo(actualDTO);
+        verify(connectionsRepository, never()).delete(Mockito.any());
+    }
+
+    @Test
+    public void testGetAllConnectionsForAUserWhenConnectionsExist() {
+        // mock data
+        Long user1 = USER1_ID;
+        Long user2 = USER2_ID;
+
+        Connection whenUser1AsToBeConnectedWith = new Connection();
+        whenUser1AsToBeConnectedWith.setCreated();
+        whenUser1AsToBeConnectedWith.setRequestingUserId(user2);
+        whenUser1AsToBeConnectedWith.setToBeConnectedWithUserId(user1);
+
+        Connection whenUser1AsRequesting = new Connection();
+        whenUser1AsRequesting.setCreated();
+        whenUser1AsRequesting.setRequestingUserId(user1);
+        whenUser1AsRequesting.setToBeConnectedWithUserId(user2);
+
+        List<ConnectOutgoingMessageDTO> expectedOutgoingMessageDTOS = new ArrayList<>();
+
+        ConnectOutgoingMessageDTO outgoingMessageWhenUser1IsToBeConnectedWith = ConnectOutgoingMessageDTO.builder()
+                .connectionSuccess(true)
+                .to(UsernameConnectionStatusDTO.builder()
+                        .userId(user2)
+                        .username(USER2_NAME)
+                        .userConnectionStatus(Constants.REQUESTING_USER)
+                        .build())
+                .message("")
+                .build();
+        expectedOutgoingMessageDTOS.add(outgoingMessageWhenUser1IsToBeConnectedWith);
+
+        ConnectOutgoingMessageDTO outGoingMessageWhenUser1IsRequesting = ConnectOutgoingMessageDTO.builder()
+                .connectionSuccess(true)
+                .to(UsernameConnectionStatusDTO.builder()
+                        .userId(user2)
+                        .username(USER2_NAME)
+                        .userConnectionStatus(Constants.TO_BE_CONNECTED_WITH_USER)
+                        .build())
+                .message("")
+                .build();
+        expectedOutgoingMessageDTOS.add(outGoingMessageWhenUser1IsRequesting);
+
+        // mock returns
+        when(connectionsRepository.findAllByToBeConnectedWithUserId(anyLong())).thenReturn(List.of(whenUser1AsToBeConnectedWith));
+        when(connectionsRepository.findAllByRequestingUserId(anyLong())).thenReturn(List.of(whenUser1AsRequesting));
+        Mockito.when(userRepository.findById(Mockito.any())).thenReturn(Optional.of(UserTestConstants.getUser2())).thenReturn(Optional.of(UserTestConstants.getUser2()));
+
+        // test
+        List<ConnectOutgoingMessageDTO> actualMessageDTOs = connectService.getAllConnectionsForAUser(user1);
+
+        assertThat(actualMessageDTOs).usingRecursiveComparison().isEqualTo(expectedOutgoingMessageDTOS);
+
+    }
+
+    @Test
+    public void testGetAllConnectionsForAUserWhenConnectionsDoNotExist() {
+        Long User1 = USER1_ID;
+
+        when(connectionsRepository.findAllByToBeConnectedWithUserId(anyLong())).thenReturn(Collections.emptyList());
+        when(connectionsRepository.findAllByRequestingUserId(anyLong())).thenReturn(Collections.emptyList());
+
+        List<ConnectOutgoingMessageDTO> actualMessageDTOs = connectService.getAllConnectionsForAUser(User1);
+
+        assertThat(actualMessageDTOs).usingRecursiveComparison().isEqualTo(Collections.emptyList());
+        verify(connectionsRepository, times(1)).findAllByToBeConnectedWithUserId(anyLong());
+        verify(connectionsRepository, times(1)).findAllByRequestingUserId(anyLong());
+        verify(userRepository, times(0)).findById(anyLong());
+    }
+
+    @Test
+    public void validateQRCodeWhenQRCodeIsEmpty() {
+        String qrcodePhrase = "";
+        Long userId = USER1_ID;
+        when(cacheService.get(any(), any())).thenReturn("");
+        boolean isValidQRCode = connectService.validateQRCode(qrcodePhrase, userId);
+        assertFalse(isValidQRCode);
+    }
+
+    @Test
+    public void validateQRCodeWhenQRCodeIsValid() {
+        String qrcodePhrase = "ABCDE";
+        Long userId = USER1_ID;
+        when(cacheService.get(any(), any())).thenReturn(qrcodePhrase);
+        boolean isValidQRCode = connectService.validateQRCode(qrcodePhrase, userId);
+        assertTrue(isValidQRCode);
+    }
+
+    @Test
+    public void validateQRCodeWhenNoQRCodeIsCached() {
+        String qrcodePhrase = "ABCDE";
+        Long userId = USER1_ID;
+        when(cacheService.get(any(), any())).thenReturn(null);
+        boolean isValidQRCode = connectService.validateQRCode(qrcodePhrase, userId);
+        assertFalse(isValidQRCode);
+    }
+
+    @Test
+    public void testConnectInvalidQRcode() {
+
+        GenericResponseDTO genericResponseDTO = GenericResponseDTO.builder()
+            .booleanMessage(false)
+            .responseMessage("Unable to validate QR code.")
+                .build();
+
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        String qrcodePhrase = "test";
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(false).when(connectServiceSpy).validateQRCode(Mockito.any(),Mockito.any());
+
+        GenericResponseDTO result = connectServiceSpy.connect(requestingUserId,toBeConnectedWithUserId,qrcodePhrase);
+
+        assertThat(genericResponseDTO).usingRecursiveComparison().isEqualTo(result);
+        verify(connectServiceSpy, never()).validateConnection(anyLong(),anyLong());
+        verify(connectionsRepository, never()).findExistingConnectionWithReversedUserIds(anyLong(),anyLong());
+        verify(connectServiceSpy,never()).saveConnectionDetails(anyLong(),anyLong());
+    }
+
+    @Test
+    public void testConnectInvalidConnection(){
+        GenericResponseDTO genericResponseDTO = GenericResponseDTO.builder()
+                .booleanMessage(false)
+                .responseMessage("message")
+                .build();
+
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        String qrcodePhrase = "test";
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(true).when(connectServiceSpy).validateQRCode(Mockito.any(),Mockito.any());
+        doReturn(Optional.of(genericResponseDTO)).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+
+        GenericResponseDTO result = connectServiceSpy.connect(requestingUserId, toBeConnectedWithUserId, qrcodePhrase);
+
+        assertThat(genericResponseDTO).usingRecursiveComparison().isEqualTo(result);
+        verify(connectionsRepository, never()).findExistingConnectionWithReversedUserIds(anyLong(),anyLong());
+        verify(connectServiceSpy,never()).saveConnectionDetails(anyLong(),anyLong());
+    }
+
+    @Test
+    public void testConnectExistingConnectionWithReverseIds(){
+
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        String qrcodePhrase = "test";
+
+        Connection existingConnection = new Connection();
+        existingConnection.setRequestingUserId(USER2_ID);
+        existingConnection.setToBeConnectedWithUserId((USER1_ID));
+
+        GenericResponseDTO genericResponseDTO = GenericResponseDTO.builder()
+                .booleanMessage(false)
+                .responseMessage("This connection already exists in reverse between the requesting user " + requestingUserId + " and the to be connected with user " + toBeConnectedWithUserId)
+                .build();
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(true).when(connectServiceSpy).validateQRCode(Mockito.any(),Mockito.any());
+        doReturn(Optional.empty()).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+        when(connectionsRepository.findExistingConnectionWithReversedUserIds(anyLong(),anyLong())).thenReturn(Optional.of(existingConnection));
+
+        GenericResponseDTO result = connectServiceSpy.connect(requestingUserId, toBeConnectedWithUserId, qrcodePhrase);
+
+        assertThat(genericResponseDTO).usingRecursiveComparison().isEqualTo(result);
+        verify(connectServiceSpy,never()).saveConnectionDetails(anyLong(),anyLong());
+    }
+
+    @Test
+    public void testConnectHappyPath(){
+
+        GenericResponseDTO genericResponseDTO = GenericResponseDTO.builder()
+                .booleanMessage(true)
+                .build();
+
+        Long requestingUserId = USER1_ID;
+        Long toBeConnectedWithUserId = USER2_ID;
+        String qrcodePhrase = "test";
+
+        ConnectService connectServiceSpy = spy(connectService);
+        doReturn(true).when(connectServiceSpy).validateQRCode(Mockito.any(),Mockito.any());
+        doReturn(Optional.empty()).when(connectServiceSpy).validateConnection(Mockito.any(),Mockito.any());
+        when(connectionsRepository.findExistingConnectionWithReversedUserIds(anyLong(),anyLong())).thenReturn(Optional.empty());
+
+        GenericResponseDTO result = connectServiceSpy.connect(requestingUserId, toBeConnectedWithUserId, qrcodePhrase);
+
+        assertThat(genericResponseDTO).usingRecursiveComparison().isEqualTo(result);
+
+    }
+}
---
+++ src/test/java/com/savvato/tribeapp/unit/services/StorageServiceImplTest.java
@@ -0,0 +1,134 @@
+package com.savvato.tribeapp.unit.services;
+
+import com.savvato.tribeapp.services.ResourceTypeService;
+import com.savvato.tribeapp.services.StorageService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+public class StorageServiceImplTest {
+    @TestConfiguration
+    static class StorageServiceTestContextConfiguration {
+        @Bean
+        public StorageService storageService() {
+            return new StorageService();
+        }
+    }
+
+    @Autowired
+    StorageService storageService;
+
+    @MockBean
+    ResourceTypeService resourceTypeService;
+
+    @Test
+    public void getDefaultFilename() {
+        String resourceType = "Profile";
+        String resourceId = "1";
+        String expectedFilename = resourceType + "Picture_" + resourceId + ".jpg";
+        String actualFilename = storageService.getDefaultFilename(resourceType, resourceId);
+        assertEquals(expectedFilename, actualFilename);
+    }
+
+    @Test
+    public void storeHappyPath() {
+        String resourceType = "profile";
+        byte[] content = new byte[1];
+        MultipartFile file = new MockMultipartFile("test file", content);
+        String directoryPathTemp = System.getProperty("java.io.tmpdir") + File.separator + "testFiles";
+        String filename = "";
+        when(resourceTypeService.getDirectoryForResourceType(anyString())).thenReturn(directoryPathTemp);
+        storageService.store(resourceType, file, filename);
+
+    }
+
+    @Test
+    public void isFileExistingHappyPath() throws IOException {
+        String resourceType = "testResourceType";
+        String filename = "testFile.txt";
+        String directoryPath = System.getProperty("java.io.tmpdir") + File.separator + "testFiles";
+
+        // Create the directory if it doesn't exist
+        File directory = new File(directoryPath);
+        directory.mkdirs();
+
+        // Create a temporary file in the specified directory
+        File tempFile = new File(directory, filename);
+        tempFile.createNewFile();
+
+        when(resourceTypeService.getDirectoryForResourceType(anyString())).thenReturn(directoryPath);
+
+        try {
+            long result = storageService.isFileExisting(resourceType, filename);
+            assertTrue(result > 0);
+        } finally {
+            // Clean up: Delete the temporary file
+            tempFile.delete();
+
+            // Clean up: Delete the temporary directory
+            directory.delete();
+        }
+    }
+
+    @Test
+    public void isFileExistingSadPath() throws IOException {
+        String resourceType = "testResourceType";
+        String filename = "testFile.txt";
+        String directoryPath = "/";
+
+        when(resourceTypeService.getDirectoryForResourceType(anyString())).thenReturn(directoryPath);
+
+        long result = storageService.isFileExisting(resourceType, filename);
+        assertTrue(result == 0);
+
+    }
+
+    @Test
+    public void deleteHappyPath() throws IOException {
+        String resourceType = "testResourceType";
+        String filename = "testFile.txt";
+        String directoryPath = System.getProperty("java.io.tmpdir") + File.separator + "testFiles";
+
+        // Create the directory if it doesn't exist
+        File directory = new File(directoryPath);
+        directory.mkdirs();
+
+        // Create a temporary file in the specified directory
+        File tempFile = new File(directory, filename);
+        tempFile.createNewFile();
+
+        when(resourceTypeService.getDirectoryForResourceType(anyString())).thenReturn(directoryPath);
+
+        try {
+            // confirm new temporary file exists
+            long result = storageService.isFileExisting(resourceType, filename);
+            if(result > 0) {
+                // delete file and confirm it is deleted
+                storageService.delete(resourceType, filename);
+                long resultAfterDelete = storageService.isFileExisting(resourceType, filename);
+                assertEquals(0, resultAfterDelete);
+            }
+        } finally {
+            // Clean up: Delete the temporary file
+            tempFile.delete();
+
+            // Clean up: Delete the temporary directory
+            directory.delete();
+        }
+    }
+}
