56f78b4f889ae5bc2712e9811146f8b79f22d1b6
Audra Wathen <mrsbluerose@gmail.com>
2024-10-03 12:18:58 -0600

    TRIB-289: adds in review to NotificationType ent

--- src/main/java/com/savvato/tribeapp/entities/NotificationType.java
+++ src/main/java/com/savvato/tribeapp/entities/NotificationType.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 public class NotificationType {
 
     public static final NotificationType ATTRIBUTE_REQUEST_APPROVED = new NotificationType(1L, "Attribute request approved", null);
+    public static final NotificationType ATTRIBUTE_REQUEST_IN_REVIEW = new NotificationType(1L, "Attribute request in review", null);
     public static final NotificationType ATTRIBUTE_REQUEST_REJECTED = new NotificationType(1L, "Attribute request rejected", null);
 
     @Id
